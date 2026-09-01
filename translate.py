996c3977fe2955e6593c1c202103366b42de19c9
sunjangyo12 <sunjangyo12@gmail.com>
2026-09-01 21:00:11 +0700

    Add external process translate with my API, path-google-translate repo

---
+++ translate.py
@@ -0,0 +1,48 @@
+import os
+import telnetlib
+import time
+
+HOST = "192.168.0.100"
+PORT = 9090
+
+tn = telnetlib.Telnet(HOST, PORT)
+
+# Tunggu sampai muncul prompt
+tn.read_until(b"Translate Server 1.0")
+
+
+all = []
+for folder, subfolders, files in os.walk("original"):
+    for file in files:
+        full = os.path.join(folder, file)
+
+        all.append(full)
+
+all.sort()
+
+for full in all:
+    print("\n"+full)
+
+    outfile = full.replace("original", "translate")
+    outpath = os.path.dirname(outfile)
+
+    os.makedirs(outpath, exist_ok=True)
+
+    with open(full, "r") as fd:
+        data = fd.read()
+
+        tn.write(data.encode('utf-8'))
+
+        for i in range(4):
+            tn.write(b"\n")
+
+        result = tn.read_until(b"\n\n============= Text Output ================\n\n\n", timeout=30)
+        result = result.decode("utf-8", errors="ignore")
+        result = result.split("============= Text Output ================")
+
+        with open(outfile, "w") as wd:
+            wd.write(result[0])
+
+        time.sleep(1)
+
+#tn.close()
