0795b74cd6885925faf7443266b67c6c4adb15d1
sunjangyo12 <sunjangyo12@gmail.com>
2026-09-02 07:11:29 +0700

    fix error encoding translate.py

--- translate.py
+++ translate.py
@@ -25,11 +25,16 @@ for full in all:
     print("\n"+full)
 
     outfile = full.replace("original", "translate")
+
+    if os.path.exists(outfile):
+        print("SKIP: "+outfile)
+        continue
+
     outpath = os.path.dirname(outfile)
 
     os.makedirs(outpath, exist_ok=True)
 
-    with open(full, "r") as fd:
+    with open(full, "r", encoding="utf-8", errors="ignore") as fd: #errors="replace"
         data = fd.read()
 
         tn.write(data.encode('utf-8'))
